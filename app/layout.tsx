3ef2820d0205204172b92417bff6b9e3986d764c
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-07-16 13:43:30 +0200

    k

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const RootLayout: React.FC<{children?: React.ReactNode}> = ({children}) => {
 	return (
 		<html lang='en'>
 			<Head>
-				<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="d2268157-8075-4fe0-a7cb-265968fd52ad" data-blockingmode="auto" type="text/javascript"></script>
+				<script id="Cookiebot" async defer src="https://consent.cookiebot.com/uc.js" data-cbid="d2268157-8075-4fe0-a7cb-265968fd52ad" data-blockingmode="auto" type="text/javascript"></script>
 			</Head>
 			<body>
 				<AppProvider>
