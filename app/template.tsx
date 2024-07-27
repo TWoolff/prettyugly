a44ee85202a4f28d8acc5228ab4d795f8bed7ad7
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-07-27 10:28:43 +0200

    custom cursor

--- app/template.tsx
+++ app/template.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
+import useCustomCursor from './utils/useCustomCursor'
 
 type TemplateProps = {
     children: React.ReactNode
 }
 
 const Template: React.FC<TemplateProps> = ({ children }) => {
+    useCustomCursor();
 
     return (
         <>
