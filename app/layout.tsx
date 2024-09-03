ad6077e539a36eb6f62914651510ef1feb4911a6
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-09-03 11:12:14 +0200

    favoicon + seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,21 @@ import './styles/index.css'
 
 export const metadata: Metadata = {
 	title: 'PrettyUgly',
-	description: 'Something Something',
+	description: 'Jewelry and accessories for the bold and the beautiful',
+	metadataBase: new URL("https://prettyugly.fashion/"),
+    openGraph: {
+        type: "website",
+        url: "https://prettyugly.fashion/",
+        title: "PrettyUgly",
+        description: "Jewelry and accessories for the bold and the beautiful",
+        locale: "en_US",
+    },
+    alternates: {
+        canonical: "/",
+    },
+    icons: {
+        icon: "/images/favicon.png",
+    },
 }
 
 const RootLayout: React.FC<{children?: React.ReactNode}> = ({children}) => {
