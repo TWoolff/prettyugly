0d4e447dfb1a1e191a93506ae826920b9289d1fc
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2025-01-02 13:48:04 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'files.stripe.com',
-      'scontent.cdninstagram.com',
-      'scontent-arn2-1.cdninstagram.com',
-      'graph.instagram.com'
-    ],
+    domains: ['files.stripe.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.ctfassets.net',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: '**.cdninstagram.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-*.cdninstagram.com',
+        pathname: '/**',
+      }
     ],
   },
 };
