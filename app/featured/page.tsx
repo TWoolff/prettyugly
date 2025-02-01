4d70d9f2bc1f73d7c1f99ce81a87c1dda6ae831f
Thomas Alexander Munch-Woolff <taw@signifly.com>
2025-02-01 11:09:00 +0100

    k

--- app/featured/page.tsx
+++ app/featured/page.tsx
@@ -4,79 +4,79 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useAppContext } from "../context";
 import Product from "../components/products/product";
 import css from "./featured.module.css";
-import { Product as ProductType } from "../types";
+import type { Product as ProductType } from "../types";
 
 interface FeaturedProduct {
-  id: string;
-  slug: string;
-  unit_amount?: number;
-  product?: {
-    active: boolean;
-    created: number;
-    default_price: string;
-    images: string[];
-    marketing_features: string[];
-    metadata: { [key: string]: string };
-    id: string;
-    name: string;
-    description: string;
-    productInfo: string;
-  };
+	id: string;
+	slug: string;
+	unit_amount?: number;
+	product?: {
+		active: boolean;
+		created: number;
+		default_price: string;
+		images: string[];
+		marketing_features: string[];
+		metadata: { [key: string]: string };
+		id: string;
+		name: string;
+		description: string;
+		productInfo: string;
+	};
 }
 
 const Featured: React.FC = () => {
-  const { state, dispatch } = useAppContext();
-  const { filters } = state;
+	const { state, dispatch } = useAppContext();
+	const { filters } = state;
 
-  useEffect(() => {
-    // Only set these filters if they're not already set
-    if (!state.filters.color) {
-      dispatch({ type: "SET_FILTER", payload: { key: "color", value: "" } });
-    }
-    if (!state.filters.category) {
-      dispatch({ type: "SET_FILTER", payload: { key: "category", value: "" } });
-    }
-  }, [dispatch, state.filters.color, state.filters.category]);
+	useEffect(() => {
+		// Only set these filters if they're not already set
+		if (!state.filters.color) {
+			dispatch({ type: "SET_FILTER", payload: { key: "color", value: "" } });
+		}
+		if (!state.filters.category) {
+			dispatch({ type: "SET_FILTER", payload: { key: "category", value: "" } });
+		}
+	}, [dispatch, state.filters.color, state.filters.category]);
 
-  const filteredProducts = state.data?.filter((product: ProductType) => {
-    return Object.entries(filters).every(([key, value]) => {
-      if (value === "") return true;
-      if (key === "featured") {
-        const featuredIds = value.split(",");
-        return featuredIds.includes(product.id);
-      }
-      return product.metadata[key] === value;
-    });
-  });
+	const filteredProducts = state.data?.filter((product: ProductType) => {
+		return Object.entries(filters).every(([key, value]) => {
+			if (value === "") return true;
+			if (key === "featured") {
+				const featuredIds = value.split(",");
+				return featuredIds.includes(product.id);
+			}
+			return product.metadata[key] === value;
+		});
+	});
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <section className={css.featured}>
-        <h1>Featured</h1>
-        <div className={css.products}>
-          <AnimatePresence>
-            {filteredProducts?.map((product: ProductType) => (
-              <motion.div key={product.id} layout>
-                <Product
-                  key={product.id}
-                  data={{
-                    ...product,
-                    currency: "",
-                    price: product.unit_amount,
-                    unit_amount: product.unit_amount,
-                    name: product.name || "",
-                    description: product.description || "",
-                    images: product.images || [],
-                    metadata: product.metadata || {},
-                  }}
-                />
-              </motion.div>
-            ))}
-          </AnimatePresence>
-        </div>
-      </section>
-    </Suspense>
-  );
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<section className={css.featured}>
+				<h1>Featured</h1>
+				<div className={css.products}>
+					<AnimatePresence>
+						{filteredProducts?.map((product: ProductType) => (
+							<motion.div key={product.id} layout>
+								<Product
+									key={product.id}
+									data={{
+										...product,
+										currency: "",
+										price: product.unit_amount || 0,
+										unit_amount: product.unit_amount || 0,
+										name: product.name || "",
+										description: product.description || "",
+										images: product.images || [],
+										metadata: product.metadata || {},
+									}}
+								/>
+							</motion.div>
+						))}
+					</AnimatePresence>
+				</div>
+			</section>
+		</Suspense>
+	);
 };
 
 export default Featured;
